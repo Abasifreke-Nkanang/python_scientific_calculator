9ae82ddd0956c1a6f1cce28d7d77ce5db4282baf
Krush-teen <Abasifrekenkanang@gmail.com>
2021-11-19 13:21:48 +0100

    pycalc.py

--- pycalc.py
+++ pycalc.py
@@ -3,4 +3,26 @@ from tkinter import*
 import math
 import tkinter.messagebox
 
+root = tk()
+
+#sets the name on the top of the gui
+root.title("Scientific Calculator")
+
+#this sets the background colour to gray-white
+root.configure(background = "graywhite")
+
+
+#sets the gui such that it cannot be resized
+root.resizable(width = False, height = False)
+
+#this sets the container named calc where all the buttons are contained in
+calc = Frame(root)
+
+#this sets the grid display of the container(buttons)
+calc.grid
+
+
+
+
+
 
