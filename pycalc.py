9454d56cba0449fed8baf5adda4335611dab7d6b
Krush-teen <Abasifrekenkanang@gmail.com>
2021-11-19 21:43:41 +0100

    placed all the buttons in their respective positions

--- pycalc.py
+++ pycalc.py
@@ -252,19 +252,216 @@ for a in range(2, 5):
         index += 1
 
 
+#Placing all the buttons, operators in their respective positions in the grid
+buttnClear = Button(calc, text = chr(67),
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = added_value.clear_entry).grid(row = 1,
+                            column = 0, pady = 1)
 
+buttnAllClear = Button(calc, text = chr(67) + chr(69),
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = added_value.all_clear_entry).grid(row = 1,
+                            column = 1, pady = 1)
+buttnsq = Button(calc, text = "\221A",
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = added_value.squareroot).grid(row = 1,
+                            column = 2, pady = 1)
+buttnAdd = Button(calc, text = "+",
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = lambda: added_value.operation("add")).grid(row = 1,
+                            column = 3, pady = 1)
+buttnSub = Button(calc, text = "-",
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = lambda: added_value.operation("sub")).grid(row = 2,
+                            column = 3, pady = 1)
+buttnMulti = Button(calc, text = "x",
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = lambda: added_value.operation("multi")).grid(row = 3,
+                            column = 3, pady = 1)
+buttnDiv = Button(calc, text = "/",
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = lambda: added_value.operation("div")).grid(row = 4,
+                            column = 3, pady = 1)
+buttnZero = Button(calc, text = "0",
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = lambda: added_value.numberEnter(0)).grid(row = 5,
+                            column = 0, pady = 1)
+buttnDot = Button(calc, text = ".",
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = lambda: added_value.numberEnter(".")).grid(row = 5,
+                            column = 1, pady = 1)
+buttnPM = Button(calc, text = chr(177),
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = lambda: added_value.mathPM).grid(row = 5,
+                            column = 2, pady = 1)
+buttnEquals = Button(calc, text = "=",
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = lambda: added_value.sum_of_total).grid(row = 1,
+                            column = 3, pady = 1)
+buttnAdd = Button(calc, text = "+",
+                            width = 6,
+                            heigth = 2,
+                            bg = "powder blue",
+                            font = ("Helvetica", 20, "bold"),
+                            bd = 4,
+                            command = lambda: added_value.operation("add")).grid(row = 1,
+                            column = 3, pady = 1)
 
+#ROW 1
+buttnPi = Button(calc, text = "pi", width = 6, height = 2,
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.pi).grid(row = 1,
+                                    column = 4, pady = 1)
+buttnCos = Button(calc, text = "cos", width = 6, height = 2,
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.cos).grid(row = 1,
+                                    column = 5, pady = 1)
+buttnSin = Button(calc, text = "sin", width = 6, height = 2,
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.sin).grid(row = 1,
+                                    column = 6, pady = 1)
+buttnTan = Button(calc, text = "tan", width = 6, height = 2,
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.tan).grid(row = 1,
+                                    column = 7, pady = 1)
 
+#ROW 2
+buttn2Pi = Button(calc, text = "2pi", width = 6, height = 2,
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.tau).grid(row = 2,
+                                    column = 4, pady = 1)
+buttnCosh = Button(calc, text = "cosh", width = 6, height = 2,
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.cosh).grid(row = 2,
+                                    column = 5, pady = 1)
+buttnSinh = Button(calc, text = "sinh", width = 6, height = 2,
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.sinh).grid(row = 2,
+                                    column = 6, pady = 1)
+buttnTanh = Button(calc, text = "tanh", width = 6, height = 2,
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.tanh).grid(row = 2,
+                                    column = 7, pady = 1)
 
+#ROW 3
+buttnLog = Button(calc, text = "log", width = 6, height = 2,
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.log).grid(row = 3,
+                                    column = 4, pady = 1)
+buttnExp = Button(calc, text = "exp", width = 6, height = 2,
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.exp).grid(row = 3,
+                                    column = 5, pady = 1)
+buttnMod = Button(calc, text = "Mod", width = 6, height = 2,
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = lambda : added_value.operation("mod")
+                                    ).grid(row = 3, column = 6, pady = 1)
+buttnE = Button(calc, text = "e", width = 6, height = 2
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.e).grid(row = 3,
+                                    column = 7, pady = 1)
 
+#ROW 4
+buttnLog10 = Button(calc, text = "log10", width = 6, height = 2
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.log10).grid(row = 4,
+                                    column = 4, pady = 1)
+buttnLog1p = Button(calc, text = "log1p", width = 6, height = 2
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.log1p).grid(row = 4,
+                                    column = 5, pady = 1)
+buttnExpm1 = Button(calc, text = "expm1", width = 6, height = 2
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.expm1).grid(row = 4,
+                                    column = 6, pady = 1)
+buttnLgamma = Button(calc, text = "lgamma", width = 6, height = 2
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.lgamma).grid(row = 1,
+                                    column = 4, pady = 1)
 
+#ROW 5
+buttnLog2 = Button(calc, text = "log2", width = 6, height = 2
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.log2).grid(row = 5,
+                                    column = 4, pady = 1)
+buttnDeg = Button(calc, text = "deg", width = 6, height = 2
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.degrees).grid(row = 5,
+                                    column = 5, pady = 1)
+buttnAcosh = Button(calc, text = "acosh", width = 6, height = 2
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.acosh).grid(row = 5,
+                                    column = 6, pady = 1)
+buttnAsinh = Button(calc, text = "asinh", width = 6, height = 2
+                                    bg = "black", fg = "white",
+                                    font = ("Helvetica", 20, "bold"),
+                                    bd = 4, command = added_value.asinh).grid(row = 5,
+                                    column = 7, pady = 1)
 
-
-
-
-
-
-
-
-
-
+lblDisplay = Label(calc, text = "Scientific Calculator", 
+                                    font = ("Helvetica", 30, "bold"),
+                                    bg = "black", fg = "white", justify = CENTER)
+lblDisplay.grid(row = 0, column = 4, columnspan = 4)
